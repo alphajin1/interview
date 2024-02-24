e99aed927ad4762b85198ba0fcabf76f3fe28ab8
alphajin1 <alphajin1@gmail.com>
2024-02-25 07:12:55 +0900

    TopSQL50 - 1934

---
+++ leetcode/TopSQL50/1934.sql
@@ -0,0 +1,22 @@
+with x as (
+    select
+        user_id,
+        sum(
+            case
+                when action = 'confirmed' then 1
+                else 0
+            end
+        ) cf_count,
+        count(*) tot_count
+    from Confirmations
+    group by user_id
+)
+
+select
+    a.user_id,
+    ROUND(IFNULL(cf_count / tot_count, 0), 2) confirmation_rate
+    from Signups a left join x
+        on a.user_id = x.user_id
+-- TODO 02-26
+-- TODO 02-27
+-- TODO 03-01
